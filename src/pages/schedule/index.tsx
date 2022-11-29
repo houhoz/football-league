68c2dd8921b58b627956b36667a363af4c1a6396
hyzhao <1048997225@qq.com>
2022-11-29 23:35:09 +0800

    feat

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 import Taro, { usePullDownRefresh } from '@tarojs/taro'
-import { Row, Col, Button } from '@nutui/nutui-react-taro'
+import { Row, Col, Button, Cell, CellGroup } from '@nutui/nutui-react-taro'
 import { getAllRound, getAllTeam } from '@/servers/league'
 import team1 from '@/assets/team1.png'
 import team2 from '@/assets/team2.png'
@@ -67,7 +67,7 @@ function Index() {
   }, [])
   return (
     <div className='schedule'>
-      <Row className='label-font'>
+      <Row className='label-font' style={{ marginBottom: 20 }}>
         <Col span='8' style={{ textAlign: 'center' }}>
           时间
         </Col>
@@ -84,61 +84,68 @@ function Index() {
       {teams.length > 0 &&
         Object.keys(rounds).length > 0 &&
         Object.keys(rounds).map(key => (
-          <div key={key}>
-            <Row key={key}>
-              <Col span='24' className='round-name'>
-                {key}
-              </Col>
-            </Row>
+          <CellGroup
+            key={key}
+            titleSlot={
+              <Row className='round-name'>
+                <Col span='24'>{key}</Col>
+              </Row>
+            }
+          >
             {rounds[key].length > 0 &&
               rounds[key].map((item, index) => {
                 const homeTeam = teams.find(v => Number(v.id) === item.homeTeam)
                 const guestTeam = teams.find(
                   v => Number(v.id) === item.guestTeam
                 )
-
                 return (
-                  <Row
+                  <Cell
                     key={index + key}
-                    className='round-row'
+                    isLink
+                    center
                     onClick={() => {
                       const { leagueId } = pageIns.current.router.params
                       Taro.navigateTo({
                         url: `/pages/match/index?matchId=${item.matchId}&leagueId=${leagueId}`,
                       })
                     }}
-                  >
-                    <Col span='8' style={{ textAlign: 'center' }}>
-                      {item.roundDate}
-                    </Col>
-                    <Col span='6'>
-                      <div
-                        className='team-info'
-                        style={{ justifyContent: 'flex-end' }}
-                      >
-                        <div>{homeTeam?.name}</div>
-                        <img
-                          style={{ marginLeft: 4 }}
-                          className='team-logo'
-                          src={homeTeam?.logo}
-                        />
-                      </div>
-                    </Col>
-                    <Col span='4' style={{ textAlign: 'center' }}>
-                      {item.finish === 1
-                        ? `${item.homeTeamGoal}: ${item.guestTeamGoal}`
-                        : '-'}
-                    </Col>
-                    <Col span='6'>
-                      <div className='team-info'>
-                        <img className='team-logo' src={guestTeam?.logo} />
-                        <div style={{ marginRight: 4 }}>{guestTeam?.name}</div>
-                      </div>
-                    </Col>
-                  </Row>
+                    title={
+                      <Row className='round-row'>
+                        <Col span='8' style={{ textAlign: 'center' }}>
+                          {item.roundDate}
+                        </Col>
+                        <Col span='6'>
+                          <div
+                            className='team-info'
+                            style={{ justifyContent: 'flex-end' }}
+                          >
+                            <div>{homeTeam?.name}</div>
+                            <img
+                              style={{ marginLeft: 4 }}
+                              className='team-logo'
+                              src={homeTeam?.logo}
+                            />
+                          </div>
+                        </Col>
+                        <Col span='4' style={{ textAlign: 'center' }}>
+                          {item.finish === 1
+                            ? `${item.homeTeamGoal}: ${item.guestTeamGoal}`
+                            : '-'}
+                        </Col>
+                        <Col span='6'>
+                          <div className='team-info'>
+                            <img className='team-logo' src={guestTeam?.logo} />
+                            <div style={{ marginRight: 4 }}>
+                              {guestTeam?.name}
+                            </div>
+                          </div>
+                        </Col>
+                      </Row>
+                    }
+                  />
                 )
               })}
-          </div>
+          </CellGroup>
         ))}
       <Button
         type='primary'
